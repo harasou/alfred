7407e8eb4d5743739fde75a2a0cec5d0a57f05c8
harasou <harasou@paperboy.co.jp>
2014-11-23 20:34:28 +0900

    Initial commit

---
+++ alfred.go
@@ -0,0 +1,43 @@
+package alfred
+
+import (
+	"encoding/xml"
+	"fmt"
+)
+
+const xmlHeader = `<?xml version="1.0" encoding="UTF-8"?>` + "\n"
+
+type Items struct {
+	Items   []Item
+	XMLName struct{} `xml:"items"`
+}
+
+type Item struct {
+	Uid          string `xml:"uid,attr"`
+	Valid        string `xml:"valid,attr"`
+	Arg          string `xml:"arg,attr"`
+	Autocomplete string `xml:"autocomplete,attr"`
+	Title        string `xml:"title"`
+	Subtitle     string `xml:"subtitle"`
+	Icon         string `xml:"icon"`
+
+	XMLName struct{} `xml:"item"`
+}
+
+func Workflow() *Items {
+	return new(Items).Init()
+}
+
+func (workflow *Items) Init() *Items {
+	workflow.Items = []Item{}
+	return workflow
+}
+
+func (workflow *Items) AddItem(item *Item) {
+	workflow.Items = append(workflow.Items, *item)
+}
+
+func (workflow *Items) Print() {
+	var xmlOutput, _ = xml.MarshalIndent(workflow, "", "  ")
+	fmt.Print(xmlHeader, string(xmlOutput))
+}
